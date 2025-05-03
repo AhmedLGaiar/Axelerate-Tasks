aca82cd6f485ac902f0ed082314ad42f3189091c
AhmedLGaiar <ahmedlgaiar@gmail.com>
2025-05-03 23:03:40 +0300

    Add project files.

---
+++ Task 1/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("60f1f9be-93da-4f38-90b5-6c32fdfed8e6")]
