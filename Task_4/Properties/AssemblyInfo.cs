d830aa623047ac9ccf0689fd7fb1ee5411e25238
AhmedLGaiar <ahmedlgaiar@gmail.com>
2025-05-06 14:36:50 +0300

    Add Task_4 project for Revit add-in functionality
    This commit introduces a new project named "Task_4" to the solution, which includes essential files and configurations for a Revit add-in. The project features a command class that interacts with Revit's API to manipulate wall elements and includes an XML configuration file. It supports multiple Revit versions and implements a singleton class for managing application data. Additionally, assembly information has been updated to include a GUID for the new project.

---
+++ Task_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("971e3ea0-f6c1-4ea8-b5f3-9e823fa08cf1")]
